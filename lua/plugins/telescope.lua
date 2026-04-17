7a80949094e34e200d89d904366b199d2469c9c9
Raul Farkas <raulfrk.wk@gmail.com>
2026-04-17 12:47:20 +0100

    feat(plugin): telescope + fzf-native with <leader>f* keymaps

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,40 @@
+-- lua/plugins/telescope.lua
+return {
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    cmd = 'Telescope',
+    keys = {
+      { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'find files' },
+      { '<leader>fg', '<cmd>Telescope git_files<cr>', desc = 'git files' },
+      { '<leader>fb', '<cmd>Telescope buffers<cr>', desc = 'buffers' },
+      { '<leader>fh', '<cmd>Telescope help_tags<cr>', desc = 'help tags' },
+      { '<leader>fr', '<cmd>Telescope oldfiles<cr>', desc = 'recent files' },
+      {
+        '<leader>fs',
+        function()
+          require('telescope.builtin').grep_string({
+            search = vim.fn.input('Grep > '),
+          })
+        end,
+        desc = 'grep string',
+      },
+      { '<leader>fw', '<cmd>Telescope live_grep<cr>', desc = 'live grep' },
+    },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    config = function()
+      local telescope = require('telescope')
+      telescope.setup({
+        defaults = {
+          path_display = { 'truncate' },
+          sorting_strategy = 'ascending',
+          layout_config = { prompt_position = 'top' },
+        },
+      })
+      telescope.load_extension('fzf')
+    end,
+  },
+}
