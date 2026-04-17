a7f86f8074246f64584c7b1d62764b2ba82f973a
Raul Farkas <raulfrk.wk@gmail.com>
2026-04-17 13:25:14 +0100

    feat(tmux): vim-tmux-navigator; relocate harpoon to <leader>h1-4

---
+++ lua/plugins/tmux-nav.lua
@@ -0,0 +1,14 @@
+-- lua/plugins/tmux-nav.lua
+return {
+  'christoomey/vim-tmux-navigator',
+  cmd = {
+    'TmuxNavigateLeft', 'TmuxNavigateDown',
+    'TmuxNavigateUp', 'TmuxNavigateRight',
+  },
+  keys = {
+    { '<C-h>', '<cmd>TmuxNavigateLeft<cr>' },
+    { '<C-j>', '<cmd>TmuxNavigateDown<cr>' },
+    { '<C-k>', '<cmd>TmuxNavigateUp<cr>' },
+    { '<C-l>', '<cmd>TmuxNavigateRight<cr>' },
+  },
+}
